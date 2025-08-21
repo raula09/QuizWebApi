d63d1b29efd27757280eade7579f1952d9ab9e7c
Raul <davitoniani0602@gmail.com>
2025-08-21 20:36:47 +0400

    testing gitignore

--- QuizWebApi/obj/Debug/net9.0/QuizWebApi.AssemblyInfo.cs
+++ QuizWebApi/obj/Debug/net9.0/QuizWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562f4b15c0afa5657b3efa7367b2344be12d50da")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
